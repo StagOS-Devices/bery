52e51cdda585043909e4586d262c2d88d74264f2
Cruzer1 <preetamswain143@gmail.com>
2022-05-21 09:47:41 +0530

    beryllium : Init Stag
    Signed-off-by: Cruzer1 <preetamswain143@gmail.com>

--- stag_beryllium.mk
+++ stag_beryllium.mk
@@ -6,11 +6,13 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Stag os stuff.
+$(call inherit-product, vendor/stag/main.mk)
+BUILD_TYPE := OFFICIAL
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := stag_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
